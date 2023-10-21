9846ac6a217449b7a249b79b85cb2fc5014cd8ee
Ana Dib <anaflavia.rdib@gmail.com>
2023-10-21 20:31:37 -0300

    feat: create quiz

---
+++ questions.js
@@ -0,0 +1,47 @@
+let questions = [
+  {
+    question: "HTML significa:",
+    options: [
+      "Hyper Transfer Markup Language",
+      "Hyper Text Makeup Language",
+      "Hyper Text Markup Language",
+      "High Transfer Markup Language]",
+    ],
+    answer: 2,
+  },
+
+  {
+    question: "Qual propriedade CSS é usada para alterar a cor do texto?",
+    options: ["color", "background-color", "text-color", "font-color"],
+    answer: 0,
+  },
+
+  {
+    question:
+      "Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?",
+    options: ['name="xxx.js"', 'src="xxx.js"', 'href="xxx.js"'],
+    answer: 1,
+  },
+
+  {
+    question: "JavaScript é uma linguagem de programação estritamente tipada.",
+    options: ["Verdade", "Falso"],
+    answer: 1,
+  },
+
+  {
+    question: "Como se declara uma função em JavaScript?",
+    options: [
+      "function = minhaFuncao() { }",
+      "function minhaFuncao() { }",
+      "myFunction() = function { }",
+    ],
+    answer: 1,
+  },
+
+  {
+    question: "Qual tag HTML é usada para criar uma lista numerada?",
+    options: ["&ltul&gt", "&ltli&gt", "&ltol&gt", "&ltdl&gt"],
+    answer: 2,
+  },
+];
